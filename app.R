af015d3c8aa54ba7af290a840f12f3d9d2293199
pedroalencar1 <pedrohlalencar@gmail.com>
2021-05-04 14:18:40 +0200

    map, legends and colour scheme

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(rworldmap)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-
+library(RColorBrewer)
 # devtools::install_github('pedroalencar1/fdClassify')
 # library(fdClassify)
 
@@ -23,20 +23,22 @@ models <- c("Mo and Lettenmeier","Ford and Labosier", "Pendergrass et al.","Nogu
             "Christian et al.", "Osman et al.")
 stations <- list.files()
 stations <- stations[substr(stations, start = 7, stop = 7) == '_']
+stations <- substr(stations,1,nchar(stations)-4)
 
 data_stations <- read.delim('stations.txt')
+user_palette <- brewer.pal(n = 9, name = 'PuBuGn')
 
 
 ui <- fluidPage(
-
+    
     # App title ----
     titlePanel('Flash Drought - Interactive Visualisation'),
-
+    
     sidebarLayout(
         # Sidebar panel for inputs ----
         sidebarPanel(
             HTML("<h3>Input parameters</h3>"),
-            selectInput("file", "Choose station and data", choices = stations, selected = 'DE-Kli_FLUXNET2015.csv'),
+            selectInput("file", "Choose station and data", choices = stations, selected = 'DE-Kli_FLUXNET2015'),
             # textOutput("file_name"),
             selectInput("Method 1", "Choose Method 1", choices = models, selected = 'Ford and Labosier'),
             selectInput("Method 2", "Choose Method 2", choices = models, selected = 'Noguera et al.'),
@@ -66,7 +68,7 @@ ui <- fluidPage(
             h6('ERA5 and FLUXNET2015 data are used to assess the relevant variables (precipitation, temperature, soil water content, actual and potential evapotranspitation'),
             h6('by Pedro Alencar (ORCID: 0000-0001-6221-8580); Code available at https://github.com/pedroalencar1/FD-Viz')
             
-            )
+        )
     )
 )
 
@@ -75,8 +77,8 @@ server <- function(input, output, session){
     
     observe({
         file1 = input$file
-        data1 = read.csv(file1)
-        name_file = unlist(strsplit(file1,'.csv'))
+        data1 = read.csv(paste(file1, '.csv', sep = ''))
+        # name_file = unlist(strsplit(file1,'.csv'))
         complete_series <- data1[,-c(1)]
         colnames(complete_series) <- c("Date","Mo and Lettenmeier","Ford and Labosier", 
                                        "Pendergrass et al.","Noguera et al." ,
@@ -91,7 +93,7 @@ server <- function(input, output, session){
         #update limits slider on the fly
         updateSliderInput(session, "range", min=min(year(complete_series$Date)))
         updateSliderInput(session, "range", max=max(year(complete_series$Date))+1)
-       
+        
         complete_series_select <- complete_series[(year(complete_series$Date) >= range[1] & year(complete_series$Date) < range[2]),] 
         
         data_station_select1 <- data_stations[which(substr(file1,start = 1, stop = 6) == data_stations$Station),]
@@ -101,12 +103,12 @@ server <- function(input, output, session){
             
             plot_ly(complete_series_select, x = ~Date) %>%
                 add_bars(y = ~precipitation, name='Prec',yaxis = 'y2', marker = list(color = 'rgba(106,90,205,1'), width = 1) %>%
-                add_lines(y = ~temperature, name='Temp', line = list(color = 'rgba(255,0,0,1')) %>%
-                add_bars(y = ~get(input$`Method 1`), name='Method 1',yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
-                add_bars(y = ~get(input$`Method 2`), name='Method 2',yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
+                add_lines(y = ~temperature, name='Temp', line = list(color = '#FC8D59')) %>%
+                add_bars(y = ~get(input$`Method 1`), name=input$`Method 1`,yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
+                add_bars(y = ~get(input$`Method 2`), name=input$`Method 2`,yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
                 
                 layout(
-                    title=paste(name_file, ': ',input$year, sep = ''),
+                    title=paste(file1, ': ',input$year, sep = ''),
                     xaxis = list(title = "Date", domain = c(0,0.95), tickmode = 'auto'),
                     yaxis = list(title = 'Temperature (ºC)', side = "left", color = "black", position = 0,
                                  anchor = 'free', range = c(-10,35), dtick = 10),
@@ -121,15 +123,15 @@ server <- function(input, output, session){
         })
         
         output$graph2 <- renderPlotly({
-
+            
             plot_ly(complete_series_select, x = ~Date) %>%
                 add_bars(y = ~precipitation, name='Prec',yaxis = 'y2', marker = list(color = 'rgba(106,90,205,1'), width = 1) %>%
-                add_lines(y = ~et0, name='ET0', line = list(color = 'rgba(255,0,0,1')) %>%
-                add_lines(y = ~eta, name='ETa', line = list(color = 'rgba(0,255,0,1')) %>%
-                add_bars(y = ~get(input$`Method 1`), name='Method 1',yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
-                add_bars(y = ~get(input$`Method 2`), name='Method 2',yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
+                add_lines(y = ~et0, name='ET0', line = list(color = '#FC8D59')) %>%
+                add_lines(y = ~eta, name='ETa', line = list(color = '#91CF60')) %>%
+                add_bars(y = ~get(input$`Method 1`), name=input$`Method 1`,yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
+                add_bars(y = ~get(input$`Method 2`), name=input$`Method 2`,yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
                 layout(
-                    title=paste(name_file, ': ',input$year, sep = ''),
+                    title=paste(file1, ': ',input$year, sep = ''),
                     xaxis = list(title = "Date", domain = c(0,0.95), tickmode = 'auto'),
                     yaxis = list(title = 'Evapotranspiration (mm/d)', side = "left", color = "black", position = 0,
                                  anchor = 'free', range = c(0,6.75), dtick = 1.5),
@@ -142,19 +144,19 @@ server <- function(input, output, session){
                     showlegend = T
                 )
         })
-
+        
         output$graph3 <- renderPlotly({
-
+            
             plot_ly(complete_series_select, x = ~Date) %>%
                 add_bars(y = ~precipitation, name='Prec',yaxis = 'y2', marker = list(color = 'rgba(106,90,205,1'), width = 1) %>%
-                add_lines(y = ~swc, name='SWC', line = list(color = 'rgba(165,42,42,1')) %>%
-                add_lines(y = ~swc_p20, name='SWC (20p)', line = list(color = 'rgba(169,169,169,1')) %>%
-                add_lines(y = ~swc_p50, name='SWC (50p)', line = list(color = 'rgba(0,0,0,1', dash ="dot")) %>%
-            
-                add_bars(y = ~get(input$`Method 1`), name='Method 1',yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
-                add_bars(y = ~get(input$`Method 2`), name='Method 2',yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
+                add_lines(y = ~swc, name='SWC', line = list(color = '#CC6666')) %>%
+                add_lines(y = ~swc_p20, name='SWC (20p)', line = list(color = 'rgba(169,169,169,1', width=1)) %>%
+                add_lines(y = ~swc_p50, name='SWC (50p)', line = list(color = 'rgba(0,0,0,1', dash ="dot",width=1)) %>%
+                
+                add_bars(y = ~get(input$`Method 1`), name=input$`Method 1`,yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
+                add_bars(y = ~get(input$`Method 2`), name=input$`Method 2`,yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
                 layout(
-                    title=paste(name_file, ': ',input$year, sep = ''),
+                    title=paste(file1, ': ',input$year, sep = ''),
                     xaxis = list(title = "Date", domain = c(0,0.95), tickmode = 'auto'),
                     yaxis = list(title = 'Soil Water Content (%)', side = "left", color = "black", position = 0,
                                  anchor = 'free', range = c(0,45), dtick = 10),
@@ -167,17 +169,17 @@ server <- function(input, output, session){
                     showlegend = T
                 )
         })
-
+        
         output$graph4 <- renderPlotly({
-
+            
             plot_ly(complete_series_select, x = ~Date) %>%
-                add_lines(y = ~spei, name='SPEI', line = list(color = 'rgba(34,139,34,1')) %>%
-                add_lines(y = ~sesr, name='SESR', line = list(color = 'rgba(255,105,180,1')) %>%
-                add_lines(y = ~eddi, name='EDDI',yaxis = 'y2', line = list(color = 'rgba(123,104,238,1')) %>%
-                add_bars(y = ~get(input$`Method 1`), name='Method 1',yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
-                add_bars(y = ~get(input$`Method 2`), name='Method 2',yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
+                add_lines(y = ~spei, name='SPEI', line = list(color =user_palette[4])) %>%
+                add_lines(y = ~sesr, name='SESR', line = list(color = user_palette[6]), visible = "legendonly") %>%
+                add_lines(y = ~eddi, name='EDDI',yaxis = 'y2', line = list(color = user_palette[8]), visible = "legendonly") %>%
+                add_bars(y = ~get(input$`Method 1`), name=input$`Method 1`,yaxis = 'y3', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
+                add_bars(y = ~get(input$`Method 2`), name=input$`Method 2`,yaxis = 'y3', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
                 layout(
-                    title=paste(name_file, ': ',input$year, sep = ''),
+                    title=paste(file1, ': ',input$year, sep = ''),
                     xaxis = list(title = "Date", domain = c(0,0.95), tickmode = 'auto'),
                     yaxis = list(title = 'SPEI and SESR', side = "left", color = "black", position = 0,
                                  anchor = 'free', range = c(-3,3), dtick = 1),
@@ -194,14 +196,14 @@ server <- function(input, output, session){
         output$graph5 <- renderPlotly({
             
             plot_ly(complete_series_select, x = ~Date) %>%
-                add_lines(y = ~et0_anomaly, name='ET0 anom.', line = list(color = 'rgba(255,0,0,1')) %>%
-                add_lines(y = ~eta_anomaly, name='ETa anom.', line = list(color = 'rgba(0,255,0,1')) %>%
-                add_lines(y = ~precipitation_anomaly, name='Prec anom.', line = list(color = 'rgba(106,90,205,1')) %>%
-                add_lines(y = ~temperature_anomaly, name='Temp anom.', line = list(color = 'rgba(218,165,32,1')) %>%
-                add_bars(y = ~get(input$`Method 1`), name='Method 1',yaxis = 'y2', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
-                add_bars(y = ~get(input$`Method 2`), name='Method 2',yaxis = 'y2', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
+                add_lines(y = ~et0_anomaly, name='ET0 anom.', line = list(color = user_palette[4])) %>%
+                add_lines(y = ~eta_anomaly, name='ETa anom.', line = list(color = user_palette[6])) %>%
+                add_lines(y = ~precipitation_anomaly, name='Prec anom.', line = list(color = user_palette[7]),visible = "legendonly") %>%
+                add_lines(y = ~temperature_anomaly, name='Temp anom.', line = list(color = user_palette[9]), visible = "legendonly") %>%
+                add_bars(y = ~get(input$`Method 1`), name=input$`Method 1`,yaxis = 'y2', marker = list(color = 'rgba(0,0,0,0.3'), width = 1) %>%
+                add_bars(y = ~get(input$`Method 2`), name=input$`Method 2`,yaxis = 'y2', marker = list(color = 'rgba(255,0,0,0.3'), width = 1) %>%
                 layout(
-                    title=paste(name_file, ': ',input$year, sep = ''),
+                    title=paste(file1, ': ',input$year, sep = ''),
                     xaxis = list(title = "Date", domain = c(0,0.95), tickmode = 'auto'),
                     yaxis = list(title = 'Anomaly (-)', side = "left", color = "black", position = 0,
                                  anchor = 'free', range = c(-3,3), dtick = 1),
@@ -213,15 +215,19 @@ server <- function(input, output, session){
         })
         
         output$graph6 <- renderLeaflet({
-          leaflet() %>% addTiles() %>%
-            addCircleMarkers(data = data_station_select1, lat = ~Latitude, lng = ~Longitude, radius = ~Duration/1.5,
-                             opacity = 1, fillOpacity = 1, color = c('black'),weight = 2, popup = ~IGPB2, fillColor = ~color1) %>%
-            addCircleMarkers(data = data_station_select2, lat = ~Latitude, lng = ~Longitude, radius = ~Duration/1.5,
-                             opacity = 0, fillOpacity = 0.33, popup = ~IGPB2, fillColor = ~color1) %>%
-            addCircleMarkers(data = data_stations, lat = ~Latitude, lng = ~Longitude, radius = 1,
-                             opacity = 0, fillOpacity = 1, popup = ~IGPB2, fillColor = 'black')
+            leaflet() %>% addTiles() %>%
+                addCircleMarkers(data = data_station_select1, lat = ~Latitude, lng = ~Longitude, radius = ~Duration/1.5,
+                                 opacity = 1, fillOpacity = 1, color = c('black'),weight = 2, popup = ~IGPB2, fillColor = ~color1) %>%
+                addCircleMarkers(data = data_station_select2, lat = ~Latitude, lng = ~Longitude, radius = ~Duration/1.5,
+                                 opacity = 0, fillOpacity = 0.33, popup = ~IGPB2, fillColor = ~color1) %>%
+                addCircleMarkers(data = data_stations, lat = ~Latitude, lng = ~Longitude, radius = 1,
+                                 opacity = 0, fillOpacity = 1, popup = ~IGPB2, fillColor = 'black')%>%
+                addLegend(position = "bottomright",
+                          colors = unique(data_stations$color1),
+                          labels = unique(data_stations$IGPB), opacity = 1,
+                          title = "Land Use")
             
-      
+            
         })
         
         
@@ -232,5 +238,3 @@ server <- function(input, output, session){
 
 
 shinyApp(ui, server)
-
-
